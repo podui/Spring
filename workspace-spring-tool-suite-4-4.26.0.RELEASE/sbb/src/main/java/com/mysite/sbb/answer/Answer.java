2830490890e87705329ce359100023371f3cf9d9
PODUI <140774372+podui@users.noreply.github.com>
2024-12-01 03:17:08 +0900

    Add files via upload

---
+++ workspace-spring-tool-suite-4-4.26.0.RELEASE/sbb/src/main/java/com/mysite/sbb/answer/Answer.java
@@ -0,0 +1,48 @@
+package com.mysite.sbb.answer;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+import com.mysite.sbb.comment.Comment;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.user.SiteUser;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(columnDefinition = "TEXT")
+	private String content;
+
+	private LocalDateTime createDate;
+
+	@ManyToOne
+	private Question question;
+
+	@ManyToOne
+	private SiteUser author;
+
+	private LocalDateTime modifyDate;
+	
+	@ManyToMany
+    Set<SiteUser> voter;
+	
+	@OneToMany(mappedBy = "answer")
+    private List<Comment> commentList;
+}
---
+++ workspace-spring-tool-suite-4-4.26.0.RELEASE/sbb/src/main/java/com/mysite/sbb/comment/Comment.java
@@ -0,0 +1,53 @@
+package com.mysite.sbb.comment;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.user.SiteUser;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    private SiteUser author;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime modifyDate;
+
+    @ManyToOne
+    private Question question;
+
+    @ManyToOne
+    private Answer answer;
+    
+    public Integer getQuestionId() {
+        Integer result = null;
+        if (this.question != null) {
+            result = this.question.getId();
+        } else if (this.answer != null) {
+            result = this.answer.getQuestion().getId();
+        }
+        return result;
+    }
+}
